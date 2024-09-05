be62691dd96186fe8aae050001d8fda07dc3d66a
Rutuja1923 <rutujajirage19@gmail.com>
2024-09-05 18:46:29 +0530

    Listening Method

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+let port = 8080;
+
+//console.log(app);
+
+app.listen(port,() => {
+    console.log(`app is listening on port ${port}`)
+});
