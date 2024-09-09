8ea5f6f01bdc4574cf9eaa210c7e45580a90dc06
Rutuja1923 <rutujajirage19@gmail.com>
2024-09-09 13:46:37 +0530

    Added a simple users route

--- index.js
+++ index.js
@@ -13,36 +13,10 @@ app.get("/",(req,res)=>{
     res.send("You contacted root path");
 });
 
-app.get("/home",(req,res)=>{
-    res.send("You contacted home path");
-});
-
-app.get("/help",(req,res)=>{
-    res.send("You contacted help path");
-});
-
-app.get("/user",(req,res)=>{
-    res.send("You contacted User path");
+app.get("/users",(req,res)=>{
+    res.send("You contacted Users path");
 });
 
 app.get("*",(req,res)=>{
     res.send("This path does not exist");
 });
-
-app.post("/",(req,res)=>{
-    res.send("You sent a POST request to root");
-});
-
-app.put('/user', (req, res) => {
-    res.send('Got a PUT request at /user');
-});
-
-app.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at /user');
-});
-
-// app.use((req,res)=>{
-//     console.log("Request Recieved");
-//     let code = "<h1>Fruits</h1><ul><li>Apple</li><li>Orange</li><li>Kiwi</li><li>Pineapple</li></ul>"
-//     res.send(code);
-// });
